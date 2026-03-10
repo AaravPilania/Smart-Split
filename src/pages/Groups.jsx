37f8dbf5b4d0a308081215f7ef80c5821aae8159
Aarav Pilania <aaravpilania2006@gmail.com>
2026-03-11 01:51:52 +0530

    Fix build: remove duplicate Navbar export, fix stray brace in Groups

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -344,7 +344,7 @@ export default function Groups() {
         </div>
       )}
 
-      {/* Create Group Modal */}}
+      {/* Create Group Modal */}
       {showCreateModal && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
           <div className="bg-white dark:bg-gray-800 rounded-xl shadow-xl max-w-md w-full p-6">
